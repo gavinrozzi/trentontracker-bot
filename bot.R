040f7f93d592467a4031493086c7f707b7135373
Gavin Rozzi <gr@gavinrozzi.com>
2021-07-30 01:35:02 -0400

    Update bot.R

--- bot.R
+++ bot.R
@@ -32,47 +32,44 @@ trentontracker_token <- create_token(
 # Get twitter mentions
 mentions <- get_mentions(token = trentontracker_token, since_id = prev_mention_id)
 
-# Stop if there are no new mentions
-if (nrow(mentions) < 1) {
-  stop('\r No new mentions to reply to.')
-}
+# Reply to new mentions if there are any to reply to
+if (nrow(mentions) >= 1) {
 
-# Loop over mentions
-for (i in 1:nrow(mentions)) {
 
-  # Break up text of each mentions into words. Check each word to see if it's a bill name.
-  for (j in 1:length(strsplit(mentions$text[i], " ")[[1]])) {
 
-    # normalize input of bill name so it's always capitalized
-    result <- toupper(strsplit(mentions$text[i], " ")[[1]][j])
+  # Loop over mentions
+  for (i in 1:nrow(mentions)) {
 
-    # If a match is found in the database, grab that bill's data
-    if (result %in% data$slug) {
-      billdata <- data %>%
-        filter(slug == result) %>%
-        left_join(acronyms, by = c("CurrentStatus" = "Acronym"))
+    # Break up text of each mentions into words. Check each word to see if it's a bill name.
+    for (j in 1:length(strsplit(mentions$text[i], " ")[[1]])) {
 
-      # Handle Bills in committee
-      if (is.na(billdata$bill_state)) {
-        bill_status <- paste(result, "-", billdata$Abstract, "\n", "Current Status:", "In Commitee", "\n", "Commitee:", billdata$FullText, "\n", "Last Action:", billdata$LDOA)
-      } else {
-        bill_status <- paste(result, "-", billdata$Abstract, "\n", "Current Status:", billdata$bill_state, "\n", "Commitee:", billdata$FullText, "\n", "Last Action:", billdata$LDOA)
+      # normalize input of bill name so it's always capitalized
+      result <- toupper(strsplit(mentions$text[i], " ")[[1]][j])
+
+      # If a match is found in the database, grab that bill's data
+      if (result %in% data$slug) {
+        billdata <- data %>%
+          filter(slug == result) %>%
+          left_join(acronyms, by = c("CurrentStatus" = "Acronym"))
+
+        # Handle Bills in committee
+        if (is.na(billdata$bill_state)) {
+          bill_status <- paste(result, "-", billdata$Abstract, "\n", "Current Status:", "In Commitee", "\n", "Commitee:", billdata$FullText, "\n", "Last Action:", billdata$LDOA)
+        } else {
+          bill_status <- paste(result, "-", billdata$Abstract, "\n", "Current Status:", billdata$bill_state, "\n", "Commitee:", billdata$FullText, "\n", "Last Action:", billdata$LDOA)
+        }
+        # Post a tweet with the bill's current status as a reply
+        post_tweet(status = bill_status, token = trentontracker_token, in_reply_to_status_id = mentions$status_id[i], auto_populate_reply_metadata = TRUE)
+        cat(paste("Replied to Tweet", mentions$status_id[i], "at", Sys.time()))
       }
-      # Post a tweet with the bill's current status as a reply
-      post_tweet(status = bill_status, token = trentontracker_token, in_reply_to_status_id = mentions$status_id[i], auto_populate_reply_metadata = TRUE)
-      cat(paste("Replied to Tweet", mentions$status_id[i], "at", Sys.time()))
     }
   }
-}
+  # Save the last mention ID so the bot can resume later.
+  # Get the last ID of the mentions to only work with tweets newer than this one on the next run
+  last_mention_id <- mentions$status_id[i]
 
-# Save the last mention ID so the bot can resume later.
-if(nrow(mentions) > 0) {
-
-# Get the last ID of the mentions to only work with tweets newer than this one on the next run
-last_mention_id <- mentions$status_id[i]
-
-# Write out the ID to a text file
-writeLines(last_mention_id, file("last_mention.txt"))
+  # Write out the ID to a text file
+  writeLines(last_mention_id, file("last_mention.txt"))
 } else {
   cat("No new mentions to reply to.")
 }
